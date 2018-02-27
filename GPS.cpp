d59217e19244fb395b9da949b91fb3b1a83df314
eberlful <markusbenedikteberl@googlemail.com>
2018-02-27 16:47:57 +0100

    ...

--- GPS.cpp
+++ GPS.cpp
@@ -1,6 +1,25 @@
 #include "GPS.h"
+#include <TinyGPS.h>
+#include <SoftwareSerial.h> 
+
+TinyGPS gps;
+float lat = 28.5458,lon = 77.1703;
 
 GPS::GPS(int pin)
 {
     pinMode(pin, OUTPUT);
+}
+
+GPS::init()
+{
+    gpsSerial.begin(9600); // connect gps sensor
+}
+
+GPS::getGPS()
+{
+    if (gpsSerial.available()){
+        if (gps.encode(gpsSerial.read())){
+            gps.f_get_position(&lat,&lon);
+        }
+    }
 }
--- GPS.h
+++ GPS.h
@@ -2,15 +2,22 @@
 #define GPS;
 
 #include "Arduion.h"
+#include <SoftwareSerial.h> 
+#include <TinyGPS.h>
 
 class GPS 
 {
     public:
         GPS(int pin);
         bool status();
+        void init();
+        void getGPS();
+        
     
     private:
         int _pin;
+        TinyGPS gps;
+        SoftwareSerial gpsSerial(3,4);//rx,tx 
 }
 
 #endif
